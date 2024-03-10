814d4d5854e9ad53f8f84f7ac7e0ccaf5986df7e
Satyam Sharma <satyamshharish@gmail.com>
2024-03-10 22:22:15 +0530

    removed consoles

--- index.js
+++ index.js
@@ -3,17 +3,11 @@ const { Server } = require("socket.io");
 const io = new Server({ cors: { origin: ["http://localhost:4200"] } });
 
 io.on("connection", (socket) => {
-    // ...
-    console.log("Connected")
     socket.on("client-event", (data) => {
+        console.log(data);
         socket.emit("server-resp", data);
     })
-    socket.emit("new-email", {sender :"server", message : "hello this message is from server"});
-
-    // setInterval(() => {
-    //     socket.emit("new-email", { message: faker.git.commitMessage(), sender: "Server" })
-    //     // socket.to("private").emit("new-email", { message: faker.git.commitMessage(), sender: "Server" })
-    // }, 5000);
+    socket.emit("Socket-Connection", {sender :"server", message : "hello this message is from server"});
 });
 
 io.listen(3000);
